7ae525eef1604d4259ef5b60b3c6d1ade91b3556
vagrant <vagrant@precise32.(none)>
2014-02-06 10:42:43 +0000

    updated some paragraph spacing and font styles

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var buddylist = require('./routes/buddylist');
 var findbuddy = require('./routes/findbuddy');
 var message = require('./routes/message');
 var schedule = require('./routes/schedule');
+var profile = require('./routes/profile');
 
 var app = express();
 
@@ -44,6 +45,7 @@ app.get('/buddylist', buddylist.view);
 app.get('/findbuddy', findbuddy.view);
 app.get('/message', message.view);
 app.get('/schedule', schedule.view);
+app.get('/profile', schedule.view);
 
 // Example route
 //app.get('/user.html', user.index);
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 /*
  * GET home page.
  */
+var data = require('../data.json');
 
 exports.view = function(req, res){
-  res.render('index');
+  res.render('index', data);
 };
