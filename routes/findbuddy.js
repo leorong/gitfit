c9fda566b08ae582d5a1bb1ee865d8841fe670cd
Akshay Buddiga <abuddiga@stanford.edu>
2014-02-12 18:58:06 -0800

    Added basic finding functionality

--- routes/findbuddy.js
+++ routes/findbuddy.js
@@ -1,6 +1,22 @@
 var data = require('../json/fake_users.json');
 
+
+
+var filter = function(users, gym) {
+	var filteredUsers = {'users':[]};
+
+	for (i = 0; i < users.length; i++) {
+		var foundUser = users[i];
+		if (foundUser['gym'] === gym) {
+			filteredUsers['users'].push(foundUser);
+		}
+	}
+	return filteredUsers;
+}
+
+var filtered = filter(data['users'], 'Arrillaga West');
+
 exports.view = function(req, res){
-  res.render('findbuddy', data);
+  res.render('findbuddy', filtered);
 };
 
