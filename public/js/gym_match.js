f35236306c8d7668f98872319d0cc007eb9a7811
leorong <leorong@stanford.edu>
2014-02-14 01:46:12 -0800

    adding inplace editing for user profile

--- public/js/gym_match.js
+++ public/js/gym_match.js
@@ -1,2 +1,9 @@
 //$("[name='find_friend_toggle']").bootstrapSwitch();
 
+$('#name').editable({
+   type:  'text',
+   pk:    1,
+   name:  'name',
+   url:   'post.php',  
+   title: 'Enter username'
+});
