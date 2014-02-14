314a9e471d5803439d475dea704943f541df8865
Akshay Buddiga <abuddiga@stanford.edu>
2014-02-14 01:15:53 -0800

    User ids

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,24 @@
 /*
  * GET home page.
  */
+
+
+
+exports.newSession = function(req, res){
+	console.log(req.query.username);
+	console.log('bah');
+	req.session.username = req.query.username;
+	req.session.lastPage = 'Login Page';
+	res.write('Last page was ' + req.session.lastPage);
+	console.log(req.session.username);
+	var obj = {};
+	obj['username'] = req.session.username;
+	var newObj = {};
+	newObj['lastPage'] = req.session.lastPage;
+	obj['pages'] = newObj;
+	res.render('index', obj);//{'username': req.session.username, 'lastPage': req.session.lastPage});
+};
+
 exports.view = function(req, res){
-  res.render('index');
+	res.render('index', {'username': req.session.username});
 };
