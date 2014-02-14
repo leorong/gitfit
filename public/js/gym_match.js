0fe99d253663c707254c298f65808d4644e535ac
leorong <leorong@stanford.edu>
2014-02-14 01:10:11 -0800

    completed profile set up and dynamically generate user profile page

--- public/js/gym_match.js
+++ public/js/gym_match.js
@@ -1,19 +1,2 @@
-$("[name='find_friend_toggle']").bootstrapSwitch();
+//$("[name='find_friend_toggle']").bootstrapSwitch();
 
-
-$('#savebtn').click(function (e) {
-	var user = {};
-	user['name'] = $('#firstname').val() + " " + $('#lastname').val();
-	user['user_name'] = $('#username').val();
-	user['age'] = $('#age').val();
-	user['gym'] = $('#gym').val();
-	user['location'] = $('#city').val() + ", " + $('#state').val();
-	user['about_me'] = $('#about_me').val();
-	user['imageURL'] = $('#image_url').val();
-	var activities = [];
-	$('#checkbox :checked').each(function() {
-		activities.push($(this).val());
-	});
-	user['activities'] = activities;
-	data['users'].push(user);
-});
--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,8 @@
+var data = require('../json/fake_users.json');
+
 exports.view = function(req, res) {
-	res.render('user');
+	var users = data['users'];
+	var randomIndex = Math.floor(Math.random() * users.length);
+	var randomUser = users[randomIndex];
+	res.render('user', randomUser);
 }
