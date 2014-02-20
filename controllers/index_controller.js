c65f2f5f7543797deebb5400182a2c46f10d759f
Akshay Buddiga <abuddiga@stanford.edu>
2014-02-19 19:57:21 -0800

    Persistent user sessions, fixed my profile page, limited app access

--- controllers/index_controller.js
+++ controllers/index_controller.js
@@ -6,36 +6,38 @@ var mongoose = require('mongoose'),
 
 /* Index page. Also populates db with fake users */
 exports.view = function(req, res) {
-
     // if (User.find({username: "rtran58"}) == None) {
     //     console.log("Empty db");
     // }
 
-    var oldUsers = data['users'];
-    for (i = 0; i < oldUsers.length; i++) {
-        var user = oldUsers[i];
-        var split = user.name.split(' ');
-        var firstName = split[0];
-        var lastName = split[1];
+    // var oldUsers = data['users'];
+    // for (i = 0; i < oldUsers.length; i++) {
+    //     var user = oldUsers[i];
+    //     var split = user.name.split(' ');
+    //     var firstName = split[0];
+    //     var lastName = split[1];
 
-        var newPerson = new User({
-            name: {first: firstName, last: lastName},
-            username: user.username,
-            email: "random@email.com",
-            password: "password",
-            age: user.age,
-            location: user.location,
-            about_me: user.about_me,
-            imageURL: user.imageURL,
-            gym: user.gym,
-            activities: user.activities,
-            looking: user.looking
+    //     var newPerson = new User({
+    //         name: {first: firstName, last: lastName},
+    //         username: user.username,
+    //         email: "random@email.com",
+    //         password: "password",
+    //         age: user.age,
+    //         location: user.location,
+    //         about_me: user.about_me,
+    //         imageURL: user.imageURL,
+    //         gym: user.gym,
+    //         activities: user.activities,
+    //         looking: user.looking
+    //     });
+    //     newPerson.save();
+    // }
+    if (!req.user) {
+        res.render('index', {user: null});
+    } else {
+        res.render('index', {
+            user: JSON.stringify(req.user),
+            'current_user': req.user.username
         });
-        newPerson.save();
     }
-
-    res.render('index', {
-        message: req.flash('info'),
-        user: req.user ? JSON.stringify(req.user) : 'null'
-    });
 }
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -6,7 +6,7 @@ var mongoose = require('mongoose'),
 
 /* Login form */
 exports.login = function(req, res) {
-    res.render('login', {user: req.user ? JSON.stringify(req.user) : 'null', 
+    res.render('login', {user: req.user ? JSON.stringify(req.user) : null, 
                         message: req.session.messages || [] });
     req.session.messages = [];
 };
@@ -58,22 +58,40 @@ exports.view = function(req, res) {
     // var users = data['users'];
     // var randomIndex = Math.floor(Math.random() * users.length);
     // var randomUser = users[randomIndex];
+
+    if (!req.user) {
+        console.log('Not logged in');
+        res.redirect('/');
+    } else {
+        console.log('We have a user');
+    }
+
     var username = req.params.username;
 
     User.findOne({username: username}, function (err, user) {
         if (err) { 
             console.log("error");
-            res.render('index');
+            res.redirect('/');
         } else {
-            res.render('user', {
-                user: req.user ? JSON.stringify(req.user) : 'null',
-                'name': user.name.full,
-                'age': user.age,
-                'imageURL': user.imageURL,
-                'location': user.location,
-                'about_me': user.about_me,
-                'activities': user.activities
-            });
+            console.log(req.user);
+            if (user) {
+                res.render('user', {
+                    user: req.user ? JSON.stringify(req.user) : null,
+                    'current_user': req.user ? req.user.username : 'null',
+                    'name': user.name.full,
+                    'username': user.username,
+                    'age': user.age,
+                    'imageURL': user.imageURL,
+                    'location': user.location,
+                    'about_me': user.about_me,
+                    'activities': user.activities
+                }); 
+            } else {
+                res.render('index', {
+                    user: req.user ? JSON.stringify(req.user) : null,
+                    'current_user': req.user ? req.user.username : 'null'
+                });
+            }
         }
     });
 
@@ -90,10 +108,10 @@ exports.buddylist = function(req, res) {
     //     }
     // });
 
-    var user = 'null';
-    if (req.user) {
-        user = JSON.stringify(req.user);
-    }
+    if (!req.user) {res.redirect('login');}
+
+    var user = JSON.stringify(req.user);
+
     var returnObj = data;
     returnObj['user'] = user;
 
