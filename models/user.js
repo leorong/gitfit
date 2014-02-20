075ec7d76769925a5b3d7c239b0e2ea2cb5ec77c
leorong <leorong@stanford.edu>
2014-02-20 01:12:18 -0800

    modified availability section

--- models/user.js
+++ models/user.js
@@ -55,36 +55,43 @@ var UserSchema = new Schema({
     }],
 
     availability: {
-        monday: [{
-            start: String,
-            end: String
-        }],
-        tuesday: [{
-            start: String,
-            end: String
-        }],
-        wednesday: [{
-            start: String,
-            end: String
-        }],
-        thursday: [{
-            start: String,
-            end: String
-        }],
-        friday: [{
-            start: String,
-            end: String
-        }],
-        saturday: [{
-            start: String,
-            end: String
-        }],
-        sunday: [{
-            start: String,
-            end: String
-        }]
+        monday: {
+            morning: Boolean,
+            afternoon: Boolean,
+            evening: Boolean
+        },
+        tuesday: {
+            morning: Boolean,
+            afternoon: Boolean,
+            evening: Boolean
+        },
+        wednesday: {
+            morning: Boolean,
+            afternoon: Boolean,
+            evening: Boolean
+        },
+        thursday: {
+            morning: Boolean,
+            afternoon: Boolean,
+            evening: Boolean
+        },
+        friday: {
+            morning: Boolean,
+            afternoon: Boolean,
+            evening: Boolean
+        },
+        saturday: {
+            morning: Boolean,
+            afternoon: Boolean,
+            evening: Boolean
+        },
+        sunday: {
+            morning: Boolean,
+            afternoon: Boolean,
+            evening: Boolean
+        }
     }
-    
+
 });
 
 /* Virtual Methods */
