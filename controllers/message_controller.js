4156b5babad4a1081d1a006e7fa1eae8ad5f4fba
vagrant <vagrant@precise32.(none)>
2014-02-21 08:11:39 +0000

    added new message function

--- controllers/message_controller.js
+++ controllers/message_controller.js
@@ -8,7 +8,7 @@ exports.view = function(req, res) {
     var user = req.user;
     console.log(user);
     
-    if (!user) {res.redirect('/login');} //res.send(500);}
+    if (!user) {res.render('/login');} //res.send(500);}
 
     console.log("message user: " + user.username + "----");
     Message
@@ -24,5 +24,35 @@ exports.view = function(req, res) {
             user: req.user ? JSON.stringify(req.user) : null,
             'current_user': req.user.username
         });
+    }
+}
+
+exports.viewForm = function(req, res) {
+    res.render('newmessage');
+}
+
+exports.addNewMessage = function(req, res) {
+    console.log("in add new message");
+    var user = req.user;
+
+    if(!user) {res.render('/login');}
+
+    var form_data = req.body;
+    var curDate = new Date();
+    console.log(form_data);
+
+    //need to check if valid to user
+    var newMessage = new Message({
+        "to": form_data.to,
+        "from": user.username,
+        "date": curDate,
+        "message": form_data.message,
+        "opened": false
+    });
+    newMessage.save(afterAdding);
+
+    function afterAdding(err) {
+        if(err) {console.log(err); res.send(500);}
+        res.redirect('/message');
     }
 }
