01bfa539f2220bf00c91854dad02dc12a0c88b67
vagrant <vagrant@precise32.(none)>
2014-03-06 09:15:32 +0000

    added modal upun adding a buddy

--- models/friend.js
+++
@@ -1,7 +0,0 @@
-var Mongoose = require('mongoose');
-
-var FriendSchema = new Mongoose.Schema({
-    "friend1": String,
-    "friend2": String,
-});
-exports.Friend = Mongoose.model('Friend', FriendSchema);
