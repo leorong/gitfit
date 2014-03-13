cb505f129297a9a540a4c3799df1437a227bc1c3
vagrant <vagrant@precise32.(none)>
2014-03-13 10:01:36 +0000

    edited timezone

--- controllers/message_controller.js
+++ controllers/message_controller.js
@@ -79,7 +79,7 @@ exports.addNewMessage = function(req, res) {
     if(!user) {res.render('/login');}
 
     var form_data = req.body;
-    var curDate = moment().tz("America/Los_Angeles").format('MMM Do YYYY, h:mm:ss a');
+    var curDate = moment().tz("America/Los_Angeles").format();
 
     // console.log(form_data);
 
